020231c9267870bf59fdb3b06ed7718009772283
James Kamz <jacquesamouzou9@gmail.com>
2023-04-27 15:16:11 +0000

    seting for static

--- swipe/settings.py
+++ swipe/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -135,6 +136,13 @@ CORS_ALLOWED_ORIGINS = [
 ]
 # User customized
 AUTH_USER_MODEL="api.CustomUser"
+
+STORAGES = {
+    # ...
+    "static": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
